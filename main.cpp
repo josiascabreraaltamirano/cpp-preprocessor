6892da520bc13a2dd58335f1739ef99f5971401a
josiascabreraaltamirano <127502262+josiascabreraaltamirano@users.noreply.github.com>
2024-01-13 22:41:43 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,71 @@ path operator""_p(const char* data, std::size_t sz) {
 }
 
 // напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+bool PreprocessInner(const path& file_path, istream& source, ostream& destine,
+					 						const vector<path>& include_directories) {
+	static regex include_version1{R"/(\s*#\s*include\s*"([^"]*)"\s*)/"};
+	static regex include_version2{R"/(\s*#\s*include\s*<([^>]*)>\s*)/"};
+
+	string line;
+	int n_line = 0;
+
+	while (getline(source, line)) {
+		++n_line;
+		smatch m1, m2;
+		if (!regex_match(line, m1, include_version1) && !regex_match(line, m2, include_version2)) {
+			destine << line << '\n';
+		} else {
+			ifstream input;
+			path include_file_name;
+			path include_file_possible_path;
+
+			if (!m1.empty()) {
+				include_file_name = string(m1[1]);
+				include_file_possible_path = file_path.parent_path() / include_file_name;
+				input.open(include_file_possible_path);
+			} else {
+				include_file_name = string(m2[1]);
+			}
+
+			for (const auto& dir : include_directories) {
+				if (input.is_open() && input) {
+					break;
+				}
+				include_file_possible_path = dir / include_file_name;
+				input.open(include_file_possible_path);
+			}
+
+			if (!input) {
+				cout << "unknown include file "s << include_file_name.string() << 
+				" at file "s << file_path.string() << " at line "s << n_line << '\n';
+				return false;
+			}
+
+			if (!PreprocessInner(include_file_possible_path, input, destine, include_directories)) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+
+}
+
+bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
+	ifstream fin(in_file);
+	if (!fin) {
+		cerr << in_file << endl;
+		return false;
+	}
+
+	ofstream fout(out_file);
+	if (!fout) {
+		cerr << out_file << endl;
+		return false;
+	}
+    
+    return PreprocessInner(in_file, fin, fout, include_directories);
+}
 
 string GetFileContents(string file) {
     ifstream stream(file);
